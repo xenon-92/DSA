c51f9e3f4867cf006a2c7cc38166bc19dac32260
axelrod92 <sumitasus92@gmail.com>
2020-07-24 14:22:31 +0530

    balanced brackets

---
+++ StringManupulation/Chapter Strings/StringManipulation/CheckParenthesis/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Write a program that checks whether the parentheses are placed
+//correctly in an arithmetic expression
+namespace CheckParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exprsn = Console.ReadLine();
+            char[] braces = { '(', ')' };
+            int leftBrace = CountBraces(exprsn, braces[0]);
+            int rightBrace = CountBraces(exprsn, braces[1]);
+            if (leftBrace == rightBrace)
+            {
+                Console.WriteLine("Balanced");
+            }
+            else
+            {
+                Console.WriteLine("UnBalanced");
+            }
+            Console.ReadKey();
+
+        }
+        static int CountBraces(string str,char brace)
+        {
+            int count = 0;
+            int index = str.IndexOf(brace);
+            while (index != -1)
+            {
+                count++;
+                index = str.IndexOf(brace,index+1);
+            }
+            return count;
+        }
+    }
+}
