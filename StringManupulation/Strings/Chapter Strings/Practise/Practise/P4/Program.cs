a59e6b7e80edb5b263cb5dc6b69a9307fa1ef357
axelrod92 <sumitasus92@gmail.com>
2020-07-26 01:07:23 +0530

    p4

---
+++ StringManupulation/Strings/Chapter Strings/Practise/Practise/P4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "one\\two\\three";
+            string[] arr = str.Split('\\');
+            string[] ararxr = str.Split();
+        }
+    }
+}
