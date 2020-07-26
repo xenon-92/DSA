be2aff52d719d565f0608775e017f7c80ad9c7c7
axelrod92 <sumitasus92@gmail.com>
2020-07-26 17:09:24 +0530

    p10

---
+++ StringManupulation/Strings/Chapter Strings/StringManipulation/P8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            StringBuilder sb = new StringBuilder(str.Length);
+            for (int i = 0; i < str.Length; i++)
+            {
+                int ascii = str[i];
+                string unicode = "\\u" + ascii.ToString("X4");
+                sb.Append(unicode);
+            }
+            Console.WriteLine(sb.ToString());
+            Console.ReadKey();
+        }
+    }
+}
