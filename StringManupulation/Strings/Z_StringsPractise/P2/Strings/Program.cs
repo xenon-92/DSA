48b780a0a67453095fc0392246fb586f7d62ef4d
xenon-92 <sumitjana92@hotmail.com>
2020-08-10 21:34:28 +0530

    s1

---
+++ StringManupulation/Strings/Z_StringsPractise/P2/Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string result = MakeReverese(str);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        private static string MakeReverese(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = str.Length-1; i >=0; i--)
+            {
+                sb.Append(str[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
