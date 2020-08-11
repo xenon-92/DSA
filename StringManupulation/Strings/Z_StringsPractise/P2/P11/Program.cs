df504891ec0fb2c0ee9c78b36de70f1ac80f7180
xenon-92 <sumitjana92@hotmail.com>
2020-08-11 09:11:13 +0530

    p11

---
+++ StringManupulation/Strings/Z_StringsPractise/P2/P11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace P11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Microsoft announced its next generation C# compiler today.It uses advanced parser and special optimizer for the Microsoft CLR.";
+            string[] forbiddenWords = { "C#","CLR","Microsoft"};
+            StringBuilder sb = new StringBuilder(str);
+            foreach (var item in forbiddenWords)
+            {
+                sb.Replace(item,string.Empty.PadRight(item.Length,'*'));
+            }
+            Console.WriteLine(sb.ToString());
+            Console.ReadKey();
+        }
+    }
+}
