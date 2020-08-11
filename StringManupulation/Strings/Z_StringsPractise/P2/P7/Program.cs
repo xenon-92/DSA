7a2a91f948b8aacdc7849803e74eabe902b57fc2
xenon-92 <sumitjana92@hotmail.com>
2020-08-11 08:22:19 +0530

    asa

---
+++ StringManupulation/Strings/Z_StringsPractise/P2/P7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(str.PadRight(20,'*'));
+            Console.ReadKey();
+        }
+    }
+}
