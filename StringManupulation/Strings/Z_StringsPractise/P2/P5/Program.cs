2695b9a675f103df90564a31c6735337df75e6fc
xenon-92 <sumitjana92@hotmail.com>
2020-08-11 07:29:23 +0530

    p5

---
+++ StringManupulation/Strings/Z_StringsPractise/P2/P5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().ToLower();
+            string find = "in";
+            int indx = str.IndexOf(find);
+            int count = 0;
+            while (indx!=-1)
+            {
+                count++;
+                indx = str.IndexOf(find,indx+1);
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
